021c1c305ead837fee7a3592af043b02060a6586
sreepathysois <sreepath.hv@manipal.edu>
2019-09-09 13:27:41 +0530

    my first commit

---
+++ MathsTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMathsLibrary;
+namespace MathsTest
+{
+[TestClass]
+public class TestMathsClass
+{
+    [TestMethod]
+    public void TestAdd()
+    {
+        //Arrange
+        CustomMaths maths = new CustomMaths(6, 5);
+
+        //Act
+        int result = maths.Add();
+
+        //Assert
+        Assert.AreEqual<int>(11, result);
+    }
+
+    [TestMethod]
+    public void TestSub()
+    {
+        //Arrange
+        CustomMaths maths = new CustomMaths(6, 5);
+
+        //Act
+        int result = maths.Sub();
+
+        //Assert
+        Assert.AreEqual<int>(12, result);
+    }
+}
+}
---
+++ MyMathsLibrary/CustomMaths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathsLibrary
+{
+    public class CustomMaths
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public CustomMaths(int num1, int num2)
+        {
+            Number1 = num1;
+            Number2 = num2;
+        }
+        public int Add()
+        {
+            return Number1 + Number2;
+        }
+        public int Sub()
+        {
+            return Number1 - Number2;
+        }
+    }
+}
